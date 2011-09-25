b85880b77f5031b77797228bc19c872c05413aaa
Thomas Levine <thomas.levine@gmail.com>
2011-09-25 13:08:20 -0400

    yay

--- countdown.sh
+++ countdown.sh
@@ -5,12 +5,12 @@ format="xml"
 #url="http://api.twitter.com/1/statuses/update."$format
 url="http://api.supertweet.net/1/statuses/update."$format
 
-seconds_diff=$((`date +%s -d "Oct 16, 2011 12:00:00 AM"`-`date +%s`))
-hours=$((($seconds_diff/3600))
-days=$(($hours/24))
+seconds_diff=$(( `date +%s -d "Oct 16, 2011 12:00:00 AM"`-`date +%s` ))
+hours=$(( $seconds_diff/3600 ))
+days=$(( $hours/24 ))
 
 #text="$1"
-text="$days days, $hours hours"
+text="$days days" #, $hours hours"
 
 echo $text
-#curl --basic --user $username:$password --data status="$text" "$url"
+curl --basic --user $username:$password --data status="$text" "$url"
