442e042a95c1329dd40f77b54e955e03daf68b6b
Thomas Levine <thomas.levine@gmail.com>
2011-09-25 13:17:49 -0400

    hours remainder

--- countdown.sh
+++ countdown.sh
@@ -6,8 +6,8 @@ format="xml"
 url="http://api.supertweet.net/1/statuses/update."$format
 
 seconds_diff=$(( `date +%s -d "Oct 16, 2011 12:00:00 AM"`-`date +%s` ))
-hours=$(( $seconds_diff/3600 ))
-days=$(( $hours/24 ))
+days=$(( $seconds_diff/(24*3600) ))
+hours=$(( ( $seconds_diff/3600 ) - ( $days * 24 ) ))
 
 #text="$1"
 #text="$days days" #, $hours hours"
